ddba35ad8d092576b37625a2a9fd0239357f3592
Maciej Tyski <maciej.tyski@pl.abb.com>
2023-03-27 16:00:50 +0200

    chore: #1 move proposed implementation from FluentResults repo

---
+++ src/Results.Immutable/Metadata/Success.cs
@@ -0,0 +1,16 @@
+﻿namespace Results.Immutable.Metadata;
+
+public record Success(
+        string Message,
+        ImmutableList<Success> Successes,
+        ImmutableDictionary<string, object> Metadata)
+    : Reason(Message, Metadata)
+{
+    public Success(string message)
+        : this(
+            message,
+            ImmutableList<Success>.Empty,
+            ImmutableDictionary<string, object>.Empty)
+    {
+    }
+}
