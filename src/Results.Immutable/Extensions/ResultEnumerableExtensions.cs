0e3fa7a0932efacdb0637cfa96d6fefd4bf5c915
mtyski <45037586+mtyski@users.noreply.github.com>
2023-03-28 15:30:11 +0200

    Revert "chore: #2 run code cleanup on the solution"
    This reverts commit 8e50e153bf0cf6ec5dbd12d0e7c2294486c5aa4c.

--- src/Results.Immutable/Extensions/ResultEnumerableExtensions.cs
+++ src/Results.Immutable/Extensions/ResultEnumerableExtensions.cs
@@ -11,5 +11,5 @@ public static class ResultEnumerableExtensions
     /// <inheritdoc cref="Result.Merge{T,TResult}" />
     /// .
     public static Result<IEnumerable<T>> Merge<T>(this IEnumerable<Result<T>> results) =>
-        Result.Merge<T, Result<T>>(results.ToList());
+        Factories.Result.Merge<T, Result<T>>(results.ToList());
 }
