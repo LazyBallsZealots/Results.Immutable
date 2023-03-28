8267d79206cd80a5405fa48422386a050d45ee91
mtyski <45037586+mtyski@users.noreply.github.com>
2023-03-28 15:30:11 +0200

    Revert "chore: #1 move proposed implementation from FluentResults repo"
    This reverts commit 27ae7e8afe8539084781459df2365969abe4cda6.

--- src/Results.Immutable.Tests/Extensions/ResultExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Results.Immutable.Tests.Extensions;
-
-internal static class ResultExtensions
-{
-    public static bool ValueMatches<T>(
-        this Result<T> result,
-        Predicate<T> predicate) =>
-        result is { Value: Some<T> { Value: var value, }, } && predicate(value);
-}
