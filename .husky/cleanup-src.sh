7760fa6f2ea833a785b4dca9891565893ccb95f9
mtyski <45037586+mtyski@users.noreply.github.com>
2023-03-28 15:30:11 +0200

    Revert "chore: #2 setup pre-commit hooks"
    This reverts commit c6eb641e0c95e388af15faf9e77d4ca1ba27fcdb.

--- .husky/cleanup-src.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-dotnet tool restore
-printf -v joined '%s;' "${@#src/}"
-dotnet jb cleanupcode --profile=FullCleanup --settings=./src/Results.Immutable.DotSettings ./src/Results.Immutable.sln "--include=${joined%;}"
