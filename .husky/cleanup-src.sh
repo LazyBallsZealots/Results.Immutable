bec45b23c5e36dc2fe03f7562d4be6b7c8e4626c
Ricardo Sánchez Encinas <gotik_tako@msn.com>
2023-05-02 17:06:29 +0200

    ci: enable precommit hooks when yarn installs
    remove pnp packages and change to node-modules

--- .husky/cleanup-src.sh
+++ .husky/cleanup-src.sh
@@ -2,4 +2,4 @@
 
 dotnet tool restore
 printf -v joined '%s;' "${@#src/}"
-dotnet jb cleanupcode --profile=FullCleanup --settings=./src/Results.Immutable.DotSettings ./src/Results.Immutable.sln "--include=${joined%;}"
+dotnet jb cleanupcode --profile=FullCleanup --settings=./src/Results.Immutable.sln.DotSettings ./src/Results.Immutable.sln "--include=${joined%;}"
