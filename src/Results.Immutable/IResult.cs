838ec733454bc9ef25c18db58b8d4e90e33c3df9
Ricardo Sanchez <ricardosancheze@outlook.com>
2023-11-14 09:07:37 +0000

    feat: add an interface IResult to allow boxing the Result

---
+++ src/Results.Immutable/IResult.cs
@@ -0,0 +1,46 @@
+using Results.Immutable.Metadata;
+
+namespace Results.Immutable;
+
+/// <summary>
+///     A structure representing the result of an operation.
+///     The usage of <see cref="Result{T}" /> is generally recommended.
+///     This interface is only useful for boxing the <see cref="IResult{T}" />.
+/// </summary>
+/// <typeparam name="T">Type of the value associated with this <see cref="IResult{T}" />.</typeparam>
+public interface IResult<T>
+{
+    /// <summary>
+    ///     The possible value of the this <see cref="IResult{T}" />.
+    /// </summary>
+    public Some<T>? Some { get; }
+
+    /// <summary>
+    ///     Gets the boolean indicator whether this <see cref="IResult{T}" />
+    ///     represents a failed operation.
+    ///     A failed operation does not contain a value, and may contain errors.
+    /// </summary>
+    public bool IsErrored { get; }
+
+    /// <inheritdoc cref="IResult{T}.IsErrored" />
+    public bool HasFailed { get; }
+
+    /// <summary>
+    ///     Gets the boolean indicator whether this <see cref="IResult{T}" />
+    ///     represents a successful operation.
+    ///     An OK operation contains a value, no errors are present.
+    /// </summary>
+    public bool IsOk { get; }
+
+    /// <inheritdoc cref="IResult{T}.IsOk" />
+    public bool HasSucceeded { get; }
+
+    /// <summary>
+    ///     Gets an <see cref="ImmutableList{T}" /> of <see cref="Error" />s
+    ///     associated with this <see cref="IResult{T}" />.
+    /// </summary>
+    /// <remarks>
+    ///     This list will be empty for successful results.
+    /// </remarks>
+    public ImmutableList<Error> Errors { get; }
+}
