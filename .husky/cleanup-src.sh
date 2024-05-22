ce3947dbc997013cf326f6a99a5a5eaffba41d03
mtyski <tyski.maciej@gmail.com>
2024-05-22 11:04:09 +0200

    chore: run code cleanup and build separately

--- .husky/cleanup-src.sh
+++ .husky/cleanup-src.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-dotnet tool restore
+dotnet tool restore && dotnet restore src/Results.Immutable.sln
+dotnet build src/Results.Immutable.sln --no-restore
 printf -v joined '%s;' "${@#src/}"
-dotnet jb cleanupcode --profile=FullCleanup --settings=./src/Results.Immutable.sln.DotSettings ./src/Results.Immutable.sln "--include=${joined%;}"
+dotnet jb cleanupcode --profile=FullCleanup --settings=./src/Results.Immutable.sln.DotSettings ./src/Results.Immutable.sln "--include=${joined%;}" --no-build
