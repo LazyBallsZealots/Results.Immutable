0592867f08288b126ba7b0bb21f74e4ac3e3d823
Ricardo Sanchez Encinas <ricardosancheze@outlook.com>
2024-06-29 11:32:39 +0200

    feat: add ParsePairs extensions
    to allow to parse each pair of a dictionary which has strings as keys

--- src/Results.Immutable.Tests/MemberTests/EachParserTests.cs
+++ src/Results.Immutable.Tests/MemberTests/EachParserTests.cs
@@ -13,12 +13,12 @@ public class EachParserTests
         var result = new[]
         {
             1,
+            2,
             3,
             4,
-            5,
         }.ParseEach(
             age => Result.OkIf(
-                age > 3,
+                age >= 3,
                 age,
                 new Error("Too young")),
             "The user should not be a toddler");
@@ -117,6 +117,120 @@ public class EachParserTests
                 {
                     7,
                     8,
+                });
+    }
+
+    [Fact(
+        DisplayName =
+            "Parses each pair of a dictionary and returns a result containing MemberErrors with inner errors created by the parser in case of failure.")]
+    public void
+        ParsesEachPairOfADictionaryAndReturnsAResultContainingMemberErrorsWithInnerErrorsCreatedByTheParserInCaseOfFailure()
+    {
+        var result = new Dictionary<string, int>
+        {
+            ["a"] = 1,
+            ["bb"] = 2,
+            ["ccc"] = 5,
+        }.ParseEachPair(
+            kv => Result.OkIf(
+                kv.Value > 3,
+                KeyValuePair.Create(kv.Key.Length, kv.Value),
+                new Error("Too young")),
+            "The user should not be a toddler");
+
+        result.Errors.Should()
+            .BeEquivalentTo(
+                ImmutableList.Create(
+                    new MemberError(
+                        "a",
+                        "The user should not be a toddler",
+                        ImmutableList.Create(new Error("Too young"))),
+                    new MemberError(
+                        "bb",
+                        "The user should not be a toddler",
+                        ImmutableList.Create(new Error("Too young")))));
+    }
+
+    [Fact(DisplayName = "Parses each pair of a dictionary successfully.")]
+    public void ParsesEachPairOfADictionarySuccessfully()
+    {
+        var result = new Dictionary<string, int>
+        {
+            ["a"] = 7,
+            ["bb"] = 8,
+        }.ParseEachPair(
+            kv => Result.OkIf(
+                kv.Value > 3,
+                KeyValuePair.Create(kv.Key.Length, kv.Value),
+                new Error("Too young")),
+            "The user should not be a toddler");
+
+        result.Should()
+            .ContainValue()
+            .Which.Should()
+            .BeEquivalentTo(
+                new Dictionary<int, int>
+                {
+                    [1] = 7,
+                    [2] = 8,
+                });
+    }
+
+    [Fact(
+        DisplayName =
+            "Parses each pair of a dictionary and returns a result containing MemberErrors with inner errors created by the parser in case of failure asynchronously.")]
+    public async Task
+        ParsesEachPairOfADictionaryAndReturnsAResultContainingMemberErrorsWithInnerErrorsCreatedByTheParserInCaseOfFailureAsync()
+    {
+        var result = await new Dictionary<string, int>
+        {
+            ["a"] = 1,
+            ["bb"] = 2,
+            ["ccc"] = 5,
+        }.ParseEachPairAsync(
+            kv => ValueTask.FromResult(
+                Result.OkIf(
+                    kv.Value > 3,
+                    KeyValuePair.Create(kv.Key.Length, kv.Value),
+                    new Error("Too young"))),
+            "The user should not be a toddler");
+
+        result.Errors.Should()
+            .BeEquivalentTo(
+                ImmutableList.Create(
+                    new MemberError(
+                        "a",
+                        "The user should not be a toddler",
+                        ImmutableList.Create(new Error("Too young"))),
+                    new MemberError(
+                        "bb",
+                        "The user should not be a toddler",
+                        ImmutableList.Create(new Error("Too young")))));
+    }
+
+    [Fact(DisplayName = "Parses each pair of a dictionary successfully asynchronously.")]
+    public async Task ParsesEachPairOfADictionarySuccessfullyAsync()
+    {
+        var result = await new Dictionary<string, int>
+        {
+            ["a"] = 7,
+            ["bb"] = 8,
+        }.ParseEachPairAsync(
+            kv => ValueTask.FromResult(
+                Result.OkIf(
+                    kv.Value > 3,
+                    KeyValuePair.Create(kv.Key.Length, kv.Value),
+                    new Error("Too young"))),
+            "The user should not be a toddler");
+
+        result.Should()
+            .ContainValue()
+            .Which.Should()
+            .BeEquivalentTo(
+                new Dictionary<int, int>
+                {
+                    [1] = 7,
+                    [2] = 8,
                 });
     }
 }
