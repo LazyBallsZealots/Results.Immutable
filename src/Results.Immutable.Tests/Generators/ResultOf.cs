c906d632d2351e6eb5d2358a5713b0b406825938
Ricardo Sanchez Encinas <ricardosancheze@outlook.com>
2024-07-03 22:05:24 +0200

    feat: return concrete type when calling `WithRootCauses` instead of `Error`
    BREAKING CHANGE: remove `Error.Create`

--- src/Results.Immutable.Tests/Generators/ResultOf.cs
+++ src/Results.Immutable.Tests/Generators/ResultOf.cs
@@ -8,7 +8,7 @@ internal static class ResultOf<T>
     public static Gen<Result<T>> Generator() =>
         Gen.OneOf(
             ArbMap.Default.GeneratorFor<ImmutableList<string>>()
-                .Select(errors => Result.Fail<T>(errors.Select(Error.Create))),
+                .Select(errors => Result.Fail<T>(errors.Select(e => new Error(e)))),
             ArbMap.Default.GeneratorFor<T>()
                 .Select(Result.Ok));
 }
--- src/Results.Immutable/Error.cs
+++ src/Results.Immutable/Error.cs
@@ -3,6 +3,8 @@
 /// <summary>
 ///     A piece of information that describes an expected failure in an operation.
 /// </summary>
+/// <param name="Message">A message that describes the error.</param>
+/// <param name="InnerErrors">A list of errors that caused this error.</param>
 public record Error(
     string Message,
     ImmutableList<Error> InnerErrors)
@@ -31,17 +33,45 @@ public record Error(
     }
 
     /// <summary>
-    ///     Creates a new <see cref="Error" /> with provided <paramref name="message" />.
+    ///     With a new <see cref="Error" /> that will be added to the existing list of inner <see cref="Error" />s.
     /// </summary>
-    /// <param name="message">The message that describes the <see cref="Error" />.</param>
-    public static Error Create(string message) => new(message);
+    /// <param name="error">Another error that caused this error to occur.</param>
+    public Error WithRootCause(Error error) =>
+        this with
+        {
+            InnerErrors = InnerErrors.Add(error),
+        };
 
+    /// <summary>
+    ///     With new <see cref="Error" />s that will be added to the existing list of inner <see cref="Error" />s.
+    /// </summary>
+    /// <param name="errors">
+    ///     A collection of errors that caused this error to occur.
+    /// </param>
+    public Error WithRootCauses(IEnumerable<Error> errors) =>
+        this with
+        {
+            InnerErrors = InnerErrors.AddRange(errors),
+        };
+}
+
+/// <summary>
+///     A piece of information that describes an expected failure in an operation.
+///     This class is intended to be used as base class for new error types.
+/// </summary>
+/// <typeparam name="TError">The type of the inheriting error.</typeparam>
+/// <inheritdoc />
+public abstract record Error<TError>(
+    string Message,
+    ImmutableList<Error> InnerErrors) : Error(Message, InnerErrors)
+    where TError : Error<TError>
+{
     /// <summary>
     ///     With a new <see cref="Error" /> that will be added to the existing list of inner <see cref="Error" />s.
     /// </summary>
-    /// <param name="message">A new message that describes the <see cref="Error" />.</param>
-    public Error WithRootCause(Error error) =>
-        this with
+    /// <param name="error">A new message that describes the <see cref="Error" />.</param>
+    public new TError WithRootCause(Error error) =>
+        (TError)this with
         {
             InnerErrors = InnerErrors.Add(error),
         };
@@ -53,8 +83,8 @@ public record Error(
     ///     A collection of new <see cref="Error" />s that will be added to the existing list of inner
     ///     <see cref="Error" />s.
     /// </param>
-    public Error WithRootCauses(IEnumerable<Error> errors) =>
-        this with
+    public new TError WithRootCauses(IEnumerable<Error> errors) =>
+        (TError)this with
         {
             InnerErrors = InnerErrors.AddRange(errors),
         };
