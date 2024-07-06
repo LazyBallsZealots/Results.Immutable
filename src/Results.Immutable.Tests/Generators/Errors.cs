e338d0ffbffdb836a9657fab441c77de48d7aeed
mtyski <tyski.maciej@gmail.com>
2024-07-06 17:14:17 +0200

    test: convert WithRootCause tests to property tests

---
+++ src/Results.Immutable.Tests/Generators/Errors.cs
@@ -0,0 +1,12 @@
+﻿using FsCheck;
+using FsCheck.Fluent;
+
+namespace Results.Immutable.Tests.Generators;
+
+internal static class Errors
+{
+    public static Gen<TError> Generator<TError>()
+        where TError : Error<TError>
+    {
+    }   
+}
