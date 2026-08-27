7756b12a526a62018ff5ba98f8497ec484f9ab89
Maciej Tyski <tyski.maciej@gmail.com>
2026-08-27 18:31:17 +0200

    feat: add AwesomeAssertions extensions for Results.Immutable

---
+++ src/Results.Immutable.Extensions.AwesomeAssertions.Tests/OptionTest.cs
@@ -0,0 +1,42 @@
+namespace Results.Immutable.Extensions.AwesomeAssertions.Tests;
+
+public sealed class OptionTest
+{
+    [Fact(DisplayName = "Contains a value with some")]
+    public void ContainsValueWithSome() =>
+        Option.Some(5)
+            .Should()
+            .ContainValue()
+            .Which.Should()
+            .Be(5);
+
+    [Fact(DisplayName = "Contains a value with none")]
+    public void ContainsValueWithNone()
+    {
+        var act = static () => Option.None<int>()
+            .Should()
+            .ContainValue("yes");
+
+        act.Should()
+            .Throw<Exception>()
+            .WithMessage("Expected option to be some because yes, but found none.");
+    }
+
+    [Fact(DisplayName = "Be none with some")]
+    public void BeNoneWithSome()
+    {
+        var act = () => Option.Some(5)
+            .Should()
+            .BeNone("yes");
+
+        act.Should()
+            .Throw<Exception>()
+            .WithMessage("Expected option to be none because yes, but found some.");
+    }
+
+    [Fact(DisplayName = "Be none with none")]
+    public void BeNoneWithNone() =>
+        Option.None<int>()
+            .Should()
+            .BeNone();
+}
---
+++ src/Results.Immutable.Extensions.AwesomeAssertions/OptionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using AwesomeAssertions.Execution;
+using Results.Immutable.Extensions.AwesomeAssertions.Assertions;
+
+namespace Results.Immutable.Extensions.AwesomeAssertions;
+
+public static class OptionExtensions
+{
+    /// <summary>
+    ///     Returns a <see cref="OptionAssertions{T}" /> for the given <see cref="Option{T}" />.
+    /// </summary>
+    /// <typeparam name="T">The type of the value of the <see cref="Option{T}" /> to assert.</typeparam>
+    public static OptionAssertions<T> Should<T>(this Option<T> option) => new(option, AssertionChain.GetOrCreate());
+}
